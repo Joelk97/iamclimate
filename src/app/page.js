a98b0a54ffd0de0754a4c43ecf0841cfb8071b21
Joelk97 <kuehljoel@gmail.com>
2023-06-26 21:43:41 +0200

    home blog events

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,91 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import "./globals.css";
+import Image from "next/legacy/image";
+import googleCalendar from "../../public/googleCalendar.png";
 
 export default function Home() {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.containerBlogEvents}>
+        <div className={`${styles.containerBlog} ${styles.containerParallel}`}>
+          <h1 className={`${styles.titleBlogEvents} ${styles.titleSection}`}>
+            Blog
+          </h1>
+          <div className={styles.blog}>
+            <p>26.06.2023</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles.blog}>
+            <p>26.06.2023</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles.blog}>
+            <p>26.06.2023</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div
+          className={`${styles.containerEvents} ${styles.containerParallel}`}
+        >
+          <h1 className={`${styles.titleBlogEvents} ${styles.titleSection}`}>
+            Eventi
+          </h1>
+          <div className={styles.event}>
+            <p>26.06.2023</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles.event}>
+            <p>26.06.2023</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.containerCalendar}>
+        <h1 className={styles.titleSection}>Disponibilità della valigia</h1>
+        <div className={styles.googleCalendarContainer}>
+          <Image src={googleCalendar} layout="fill" objectFit="contain" />
+        </div>
+      </div>
+    </div>
+  );
 }
