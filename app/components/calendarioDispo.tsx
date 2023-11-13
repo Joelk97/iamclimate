ff4f006a54dcbea7e5c9caa226b598f43c5f7bd4
Joelk97 <93705438+Joelk97@users.noreply.github.com>
2023-11-13 16:01:55 +0100

    width calendar

--- app/components/calendarioDispo.tsx
+++ app/components/calendarioDispo.tsx
@@ -10,8 +10,7 @@ const Disponibilita = () => {
       <h1 className="text-5xl dark:text-black">{t("dispo")}</h1>
       <iframe
         src="https://calendar.google.com/calendar/embed?height=600&wkst=2&%23ffffff&ctz=Europe%2FZurich&showTz=1&showCalendars=1&src=NGViYjgyYTg3ZmM1ZjkzZjY1MWM0MzUwZjQ3NzAyNzAyMjZiNTI2N2M1ZjRkMTBkMGM1MGZkN2NlMTA2MWU5Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
-        style={{ border: "0px", borderRadius: "4px" }}
-        width="1000"
+        style={{ border: "0px", borderRadius: "4px", width: "90%" }}
         height="500"
       ></iframe>
       <button className="px-6 py-3 rounded-xl text-2xl dark:text-white dark:bg-black text-black bg-white">
